0413dae8491ad7e93d3b02f37a985badb86e1bcc
Frédéric Cabestre <frederic.cabestre@sigusr.net>
2020-12-05 17:55:21 +0100

    Add more slides about the classical F-Bounded inheritance method

--- code/C#/Typeclasses/FBounded/OrdTest.cs
+++ code/C#/Typeclasses/FBounded/OrdTest.cs
@@ -11,18 +11,42 @@ namespace Typeclasses.FBounded
         [Test]
         public void IntOrdTest()
         {
-            var intOrds = new List<IntOrdered> {new IntOrdered {Value = 1}, new IntOrdered {Value = -4}, new IntOrdered {Value = 42}, new IntOrdered {Value = 12}};
+            var intOrds = new List<IntOrdered>
+            {
+                new IntOrdered {Value = 1}, 
+                new IntOrdered {Value = -4}, 
+                new IntOrdered {Value = 42}, 
+                new IntOrdered {Value = 12}
+            };
             var actual = Sort(intOrds);
-            var expected = new List<IntOrdered> {new IntOrdered {Value = -4}, new IntOrdered {Value = 1}, new IntOrdered {Value = 12}, new IntOrdered {Value = 42}};
+            var expected = new List<IntOrdered>
+            {
+                new IntOrdered {Value = -4}, 
+                new IntOrdered {Value = 1}, 
+                new IntOrdered {Value = 12}, 
+                new IntOrdered {Value = 42}
+            };
             Assert.IsTrue(Equality(actual, expected));
         }
 
         [Test]
         public void StringOrdTest()
         {
-            var stringOrds = new List<StringOrdered> {new StringOrdered {Value = "a"}, new StringOrdered {Value = "z"}, new StringOrdered {Value = "e"}, new StringOrdered {Value = "r"}};
+            var stringOrds = new List<StringOrdered>
+            {
+                new StringOrdered {Value = "a"}, 
+                new StringOrdered {Value = "z"}, 
+                new StringOrdered {Value = "e"}, 
+                new StringOrdered {Value = "r"}
+            };
             var actual = Sort(stringOrds);
-            var expected = new List<StringOrdered> {new StringOrdered {Value = "a"}, new StringOrdered {Value = "e"}, new StringOrdered {Value = "r"}, new StringOrdered {Value = "z"}};
+            var expected = new List<StringOrdered>
+            {
+                new StringOrdered {Value = "a"}, 
+                new StringOrdered {Value = "e"}, 
+                new StringOrdered {Value = "r"}, 
+                new StringOrdered {Value = "z"}
+            };
             Assert.IsTrue(Equality(actual, expected));
         }
     }
