937feb4d1be35ab435705eec5924e50e6a9164ac
Frédéric Cabestre <frederic.cabestre@sigusr.net>
2020-12-07 20:49:07 +0100

    Add first Haskell slides

--- code/Haskell/src/Algorithm.hs
+++ code/Haskell/src/Algorithm.hs
@@ -8,3 +8,6 @@ insert elem (h:t) = if gt elem h then h:(insert elem t) else elem:h:t
 
 sort :: Ordered a => [a] -> [a]
 sort = foldr insert [] 
+
+equality :: Ordered a => [a] -> [a] -> Bool
+equality list1 list2 = all (\(l,r) -> eq l r) (zip list1 list2)
--- code/Haskell/test/OderedSpec.hs
+++ code/Haskell/test/OderedSpec.hs
@@ -7,10 +7,10 @@ main :: IO ()
 main = hspec $ do
   describe "Ordered" $ do
     it "IntOrd" $
-      (sort [1, -4, 42, 12]::[Integer]) `shouldBe` [-4,1,12,42]
+      (equality (sort [1, -4, 42, 12]::[Integer]) [-4, 1, 12, 42]) `shouldBe` True 
 
     it "StringOrd" $
-      (sort ["a", "z", "e", "r"]::[String]) `shouldBe` ["a","e","r","z"]
+      (equality (sort ["a", "z", "e", "r"]::[String]) ["a", "e", "r", "z"]) `shouldBe` True
 
     it "PairOrd" $
-      (sort [(1, "a"), (-4, "z"), (-4, "e"), (42, "r")]::[(Integer, String)]) `shouldBe`  [(-4,"e"),(-4,"z"),(1,"a"),(42,"r")]
+      (equality (sort [(1, "a"), (-4, "z"), (-4, "e"), (42, "r")]::[(Integer, String)]) [(-4,"e"), (-4,"z"), (1,"a"), (42,"r")]) `shouldBe` True
