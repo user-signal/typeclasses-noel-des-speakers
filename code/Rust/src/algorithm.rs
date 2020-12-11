faed5ec845d9af6fc03fc1f26ef6c699805c1c9b
Frédéric Cabestre <frederic.cabestre@sigusr.net>
2020-12-11 17:43:17 +0100

    Complete Haskell and Rust slides + ressources

--- code/Rust/src/algorithm.rs
+++ code/Rust/src/algorithm.rs
@@ -2,17 +2,21 @@ use crate::ord::Ordered;
 
 pub fn sort<T: Ordered + Clone>(source: &Vec<T>) -> Vec<T> {
     let mut result: Vec<T> = Vec::new();
-    for source_elem in source {
+    for s in source {
         let position =
-            match result.iter().position(|sorted_elem| Ordered::gt(sorted_elem, &source_elem)) {
+            match result.iter().position(|r| Ordered::gt(r, &s)) {
                 None => result.len(),
                 Some(p) => p
             };
-        result.insert(position, source_elem.clone());
+        result.insert(position, s.clone());
     }
     return result;
 }
 
 pub fn equality<T: Ordered>(vector1: &Vec<T>, vector2: &Vec<T>) -> bool {
-    vector1.len() == vector2.len() && vector1.iter().zip(vector2.iter()).all(|p| Ordered::eq(p.0, p.1))
+    vector1.len() == 
+        vector2.len() && 
+        vector1.iter()
+            .zip(vector2.iter())
+            .all(|p| Ordered::eq(p.0, p.1))
 }
--- code/Rust/src/ord.rs
+++ code/Rust/src/ord.rs
@@ -14,6 +14,10 @@ impl Ordered for &str {
 }
 
 impl<A: Ordered, B: Ordered> Ordered for (A, B) {
-    fn gt(l: &Self, r: &Self) -> bool { A::gt(&l.0, &r.0) || (A::eq(&l.0, &r.0) && B::gt(&l.1, &r.1))}
-    fn eq(l: &Self, r: &Self) -> bool { A::eq(&l.0, &r.0) && B::eq(&l.1, &r.1) }
+    fn gt(l: &Self, r: &Self) -> bool {
+        A::gt(&l.0, &r.0) || (A::eq(&l.0, &r.0) && B::gt(&l.1, &r.1))
+    }
+    fn eq(l: &Self, r: &Self) -> bool {
+        A::eq(&l.0, &r.0) && B::eq(&l.1, &r.1)
+    }
 }
